b9e5b1c3e9cb25d01f1d76bdba2c8bba56b6b2cc
jchristian1 <jchristian_1@hotmail.com>
2022-02-01 02:21:37 -0500

    updating static_root

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'playground/static']
-#STATIC_ROOT = os.path.join(BASE_DIR,'playground/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'playground/staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
