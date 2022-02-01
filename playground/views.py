dabb2d3a3e7e6ad213d5958a0626883e263bdfd8
jchristian1 <jchristian_1@hotmail.com>
2022-02-01 00:43:59 -0500

    static files errors solved

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def say_hello(request):
-    return render(request,'hello.html',{'name':'Chris'})
+def index(request):
+    return render(request,'index.html',{'name':'Chris'})
