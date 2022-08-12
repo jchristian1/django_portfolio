b1c0424efb7a7b127a8e152bedcafd17390b73c7
jchristian1 <jchristian_1@hotmail.com>
2022-08-12 12:06:32 -0400

    Uploading to heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -127,9 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/playground/static/'
 STATICFILES_DIRS = [    
     os.path.join(BASE_DIR,'playground/static/'),
-]
-print( STATICFILES_DIRS[0])
-print(STATIC_URL) 
+] 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
