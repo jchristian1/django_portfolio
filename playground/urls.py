442c4063ff15aab45732b7a71f2793c1b5a44d9b
jchristian1 <jchristian_1@hotmail.com>
2022-08-16 19:50:46 -0400

    machine learning models client set up, changes in bio

--- playground/urls.py
+++ playground/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='home'),
-    path('cyclist',views.cyclist_project,name='cyclist')
+    path('cyclist',views.cyclist_project,name='cyclist'),
+    path('sentimenter',views.sentimenter,name='sentimenter'),
+    path('spammer',views.spammer,name='spammer')
 ]
--- playground/views.py
+++ playground/views.py
@@ -5,3 +5,7 @@ def index(request):
     return render(request,'index.html')
 def cyclist_project(request):
     return render(request, 'cyclistic.html')
+def sentimenter(request):
+    return render(request, 'sentimenter.html')
+def spammer(request):
+    return render(request, 'spammer.html')
